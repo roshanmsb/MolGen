999fd7dee9776dae9f7012ad439e8f7ec73ef318
Roshan <bt21d401@smail.iitm.ac.in>
2023-10-24 15:39:13 +0530

    Update models

--- models/gpt2.py
+++ models/gpt2.py
@@ -1,31 +1,69 @@
-import torch
-import torch.nn as nn
-from transformers import GPT2Model, GPT2Config
+import pytorch_lightning as pl
+from torch.optim import AdamW
+from transformers import (
+    AutoConfig,
+    GPT2LMHeadModel,
+    get_linear_schedule_with_warmup,
+)
 
 
-class GPT2LMHeadModel(nn.Module):
-    def __init__(self, config):
+class GPT2MolGen(pl.LightningModule):
+    def __init__(
+        self,
+        model_name_or_path,
+        max_seq_length,
+        vocab_size,
+        bos_token_id,
+        eos_token_id,
+        lr,
+        weight_decay,
+        warmup_steps,
+    ):
         super().__init__()
-        self.transformer = GPT2Model(config)
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-        self.init_weights()
+        self.save_hyperparameters()
 
-    def init_weights(self):
-        self.lm_head.weight = self.transformer.wte.weight
+        config = AutoConfig.from_pretrained(
+            model_name_or_path,
+            vocab_size=vocab_size,
+            n_ctx=max_seq_length,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+        )
+        self.model = GPT2LMHeadModel(config=config)
+        self.lr = lr
+        self.weight_decay = weight_decay
+        self.warmup_steps = warmup_steps
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
-                inputs_embeds=None, labels=None):
-        transformer_outputs = self.transformer(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids,
-                                                position_ids=position_ids, head_mask=head_mask,
-                                                inputs_embeds=inputs_embeds)
-        hidden_states = transformer_outputs[0]
-        lm_logits = self.lm_head(hidden_states)
+    def forward(self, x):
+        return self.model(
+            input_ids=x["input_ids"],
+            attention_mask=x["attention_mask"],
+            labels=x["labels"],
+        )
 
-        outputs = (lm_logits,) + transformer_outputs[1:]
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
-            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
-            outputs = (loss,) + outputs
+    def training_step(self, batch, batch_idx):
+        loss = self(batch).loss
+        self.log(
+            "train_loss", loss, on_epoch=True, on_step=True, sync_dist=True
+        )
+        return loss
 
-        return outputs
+    def validation_step(self, batch, batch_idx):
+        loss = self(batch).loss
+        self.log(
+            "valid_loss", loss, on_step=True, on_epoch=True, sync_dist=True
+        )
+
+    def configure_optimizers(self):
+        optimizer = AdamW(
+            self.trainer.model.parameters(),  # type: ignore
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=self.warmup_steps,
+            num_training_steps=self.trainer.estimated_stepping_batches,
+        )
+        return [optimizer], [scheduler]
